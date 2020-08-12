c41a6e32e935ba511420ba4717f8d76f18c1911a
zerformza5566 <69533450+zerformza5566@users.noreply.github.com>
2020-08-13 01:35:08 +0700

    Execise5-1

---
+++ assignment/Exercise5_1_Peerapun_S.py
@@ -0,0 +1,12 @@
+firstNumber = float(input("1st number : "))
+secondNumber = float(input("2nd number : "))
+
+plus = firstNumber + secondNumber
+minus = firstNumber - secondNumber
+multiply = firstNumber * secondNumber
+divide = firstNumber / secondNumber
+
+print(firstNumber, "+", secondNumber, "=", plus)
+print(firstNumber, "-", secondNumber, "=", minus)
+print(firstNumber, "*", secondNumber, "=", multiply)
+print(firstNumber, "/", secondNumber, "=", divide)
