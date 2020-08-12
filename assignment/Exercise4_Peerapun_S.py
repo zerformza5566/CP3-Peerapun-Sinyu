bb28c133356acfe9df9b1fd6e8b79200494c103c
zerformza5566 <69533450+zerformza5566@users.noreply.github.com>
2020-08-12 23:52:17 +0700

    exercise 4

---
+++ assignment/Exercise4_Peerapun_S.py
@@ -0,0 +1,10 @@
+engScore = 83
+busiScore = 71
+comSysScore = 78
+comProScore = 92
+
+print("----Your Score----")
+print("Foundation English :", engScore)
+print("General Business :", busiScore)
+print("Introduction to Computer Systems :", comSysScore)
+print("Computer Programming :", comProScore)
