e74ed9903bf0ae71825f007e9cea520acce822a5
zerformza5566 <69533450+zerformza5566@users.noreply.github.com>
2020-08-14 20:27:24 +0700

    Lecture72

---
+++ assignment/Lecture72_Peerapun_S.py
@@ -0,0 +1,20 @@
+menuList = []
+
+def showBill():
+    totalPrice = 0
+    print("---- Food ----")
+    for number in range(len(menuList)):
+        print(menuList[number])
+        totalPrice += menuList[number][1]
+    print("Total :", totalPrice)
+
+while True:
+    menuName = input("Plese Enter Menu : ")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = int(input("Price : "))
+        menuList.append([menuName,menuPrice])
+
+
+showBill()
