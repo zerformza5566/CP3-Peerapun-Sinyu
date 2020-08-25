787a7c96d06bf1e70ec7b977fdc62c0205724059
zerformza5566 <69533450+zerformza5566@users.noreply.github.com>
2020-08-25 23:37:50 +0700

    8/25/2020

--- projects/gui/banking gui/banking gui.py
+++ projects/gui/banking gui/banking gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import os
 from PIL import ImageTk, Image
 
@@ -14,7 +15,9 @@ def toggle_password():
 def register():
     register_window = Toplevel(master)
     register_window.title("Register")
-
+    global temp_name
+    global temp_age
+    global temp_password
     temp_name = StringVar()
     temp_age = StringVar()
     temp_password = StringVar()
@@ -23,10 +26,11 @@ def register():
     txt_label.grid(row=0, sticky=W, pady=10, padx=5)
     name_label = Label(register_window, text="Name :", font=("Calibri", 12))
     name_label.grid(row=1, sticky=W)
-    age_label = Label(register_window, text="Age :", font=("Calibri", 12))
-    age_label.grid(row=2, sticky=W)
     password_label = Label(register_window, text="Password :", font=("Calibri", 12))
-    password_label.grid(row=3, sticky=W)
+    password_label.grid(row=2, sticky=W)
+    age_label = Label(register_window, text="Age :", font=("Calibri", 12))
+    age_label.grid(row=3, sticky=W)
+
 
     global password_entry
     global toggle_check
@@ -34,17 +38,37 @@ def register():
     check_var = IntVar()
     name_entry = Entry(register_window, textvariable=temp_name)
     name_entry.grid(row=1, column=0)
-    age_entry = Entry(register_window, textvariable=temp_age)
-    age_entry.grid(row=2, column=0)
     password_entry = Entry(register_window, textvariable=temp_password, show="*")
-    password_entry.grid(row=3, column=0)
+    password_entry.grid(row=2, column=0)
+    age_entry = Entry(register_window, textvariable=temp_age)
+    age_entry.grid(row=3, column=0)
+
     toggle_check = Checkbutton(register_window, text="Show", variable=check_var,
                                onvalue=1, offvalue=0, command=toggle_password)
-    toggle_check.grid(row=3, sticky=E)
+    toggle_check.grid(row=2, sticky=E)
     Button(register_window, text="Register", command=finish_regis, font=("Calibri", 12)).grid(row=4, sticky=N, pady=10)
 
 def finish_regis():
-    pass
+    name = temp_name.get()
+    age = temp_age.get()
+    password = temp_password.get()
+    all_accounts = os.listdir()
+
+    if name == "" or age == "" or password == "":
+        messagebox.showwarning("Warning", "All fields required")
+        return
+
+    for name_check in all_accounts:
+        if name == name_check:
+            messagebox.showwarning("Warning", "Account already exists")
+            return
+        else:
+            new_file = open(name, "w")
+            new_file.write(name + "\n")
+            new_file.write(password + "\n")
+            new_file.write(age + "\n")
+            new_file.close()
+    messagebox.showinfo("Completed", "Account has been created!")
 
 def login():
     pass
